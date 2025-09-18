4f256a80c46e466b87b731fa175b55960fd2b2a6
Matija-Kukic <kukicmatija@gmail.com>
2025-09-18 14:27:43 +0200

    Code from stm32Cube done.

---
+++ tmp/STM32CubeIDe/WiFi_Client_Server/STM32CubeIDE/Debug/Application/WIFI/es_wifi.d
@@ -0,0 +1,5 @@
+Application/WIFI/es_wifi.o: \
+ /home/matijak/programiranje/spyrosoft/STM32CubeIDE/WiFi_Client_Server/Common/Src/es_wifi.c \
+ ../../Common/Inc/es_wifi.h ../../Inc/es_wifi_conf.h
+../../Common/Inc/es_wifi.h:
+../../Inc/es_wifi_conf.h:
--- tmp/tcp/tcp_client_transfer.py
+++ tmp/tcp/tcp_client_transfer.py
@@ -4,23 +4,27 @@ import socket
 def tcp_client(host: str, port: int):
     # Create TCP socket
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        with open("blinky_linked.bin", "rb") as f:
-            print(f"Connecting to {host}:{port}...")
-            s.connect((host, port))
-            print("Connected! Type messages and press Enter. Type 'quit' to exit.\n")
-            arr = b"\x00\xAA\xBB\xCC\xDD\x00"
-            try:
-                while True:
-                    num = int(input("> "))
-                    message = b""
-                    if num == 0:
-                        message = b"\x00\x48\x65\x6C\x6C\x6F"
-                    if num == 1:
+        full_file = b""
+        print(f"Connecting to {host}:{port}...")
+        s.connect((host, port))
+        print("Connected! Type messages and press Enter. Type 'quit' to exit.\n")
+        # arr = b"\x00\xAA\xBB\xCC\xDD\x00"
+        try:
+            while True:
+                num = int(input("> "))
+                message = b""
+                if num == 0:
+                    message = b"\x00\x48\x65\x6C\x6C\x6F"
+                if num == 1:
+                    with open("binaries/WIFI_OTA_2.bin", "rb") as f:
                         while True:
                             chunk = f.read(512)
                             if not chunk:
                                 break
+                            if len(chunk) < 512:
+                                chunk += b"\x00" * (512 - len(chunk))
                             message = b"\x01" + chunk
+                            full_file += chunk
                             s.sendall(message)
                             data = s.recv(4096)
                             if not data:
@@ -29,29 +33,56 @@ def tcp_client(host: str, port: int):
                             print(f"Received: {data.decode(errors='replace')}")
                         continue
 
-                    if num == 2:
-                        message = b"\x02\x42\x79\x65\x2E"
+                if num == 2:
+                    import binascii
 
-                    # Send message
+                    checksum = binascii.crc32(full_file) & 0xFFFFFFFF
+                    checksum_bytes = checksum.to_bytes(4, byteorder="little")
+                    message = b"\x02" + checksum_bytes
+
+                    full_file = b""
+                    print(f"crc32 = {checksum:08x}")
+                if num == 3:
+                    import binascii
+
+                    checksum = binascii.crc32(full_file) & 0xFFFFFFFF
+                    checksum_bytes = checksum.to_bytes(4, byteorder="little")
+                    checksum_bytes = b"\xFF" * 4 + checksum_bytes[4:]
+                    message = b"\x02" + checksum_bytes
+
+                    full_file = b""
+                    print(f"crc32 = {checksum:08x}")
+
+                if num == 4:
+                    for i in range(12):
+                        message = b"\x01" + b"\xAA" * 512
+                        s.sendall(message)
+                        data = s.recv(4096)
+                        if not data:
+                            print("Server closed the connection.")
+                            break
+                        print(f"Received: {data.decode(errors='replace')}")
+                    continue
+                if num == 5:
+                    message = b"\xAA" * 5
                     s.sendall(message)
 
-                    # Receive response (up to 4 KB)
-                    data = s.recv(4096)
+                    break
 
-                    if not data:
-                        print("Server closed the connection.")
-                        break
-                    print(f"Received: {data.decode(errors='replace')}")
-                    rec = data.decode(errors="replace")
-                    if "Finished." in rec:
-                        print("Finished transfer")
-                        break
-                    elif "Confirm." not in rec:
-                        print(f"BAD RECEPTION")
-                        break
+                # Send message
+                s.sendall(message)
 
-            except KeyboardInterrupt:
-                print("\nInterrupted by user, closing connection.")
+                # Receive response (up to 4 KB)
+                data = s.recv(4096)
+
+                if not data:
+                    print("Server closed the connection.")
+                    break
+                print(f"Received: {data.decode(errors='replace')}")
+                rec = data.decode(errors="replace")
+
+        except KeyboardInterrupt:
+            print("\nInterrupted by user, closing connection.")
 
 
 if __name__ == "__main__":
