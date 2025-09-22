3f02b25100bc61a46e3b61cea7207107433d49c0
NiklaMag <nm011905830@fer.hr>
2025-09-22 09:19:25 -0400

    Add NewFirmware/WIFI_OTA_2 (STM32CubeIDE project)

---
+++ Application/NewFirmware/WIFI_OTA_2/Build_Wifi_Ota/CMakeFiles/WIFI_OTA_2.dir/Common/Src/wifi.c.obj.d
@@ -0,0 +1,60 @@
+CMakeFiles/WIFI_OTA_2.dir/Common/Src/wifi.c.obj: \
+ /home/student/Desktop/final_files1/tmp/STM32CubeIDe/WIFI_OTA_2/Common/Src/wifi.c \
+ /home/student/Desktop/final_files1/tmp/STM32CubeIDe/WIFI_OTA_2/Common/Inc/wifi.h \
+ /home/student/Desktop/final_files1/tmp/STM32CubeIDe/WIFI_OTA_2/Common/Inc/es_wifi.h \
+ /usr/lib/gcc/arm-none-eabi/10.3.1/include/stdint.h \
+ /usr/include/newlib/string.h /usr/include/newlib/_ansi.h \
+ /usr/include/newlib/newlib.h /usr/include/newlib/_newlib_version.h \
+ /usr/include/newlib/sys/config.h /usr/include/newlib/machine/ieeefp.h \
+ /usr/include/newlib/sys/features.h /usr/include/newlib/sys/reent.h \
+ /usr/include/newlib/_ansi.h \
+ /usr/lib/gcc/arm-none-eabi/10.3.1/include/stddef.h \
+ /usr/include/newlib/sys/_types.h /usr/include/newlib/machine/_types.h \
+ /usr/include/newlib/machine/_default_types.h \
+ /usr/include/newlib/sys/lock.h /usr/include/newlib/sys/cdefs.h \
+ /usr/include/newlib/sys/_locale.h /usr/include/newlib/strings.h \
+ /usr/include/newlib/sys/string.h \
+ /usr/lib/gcc/arm-none-eabi/10.3.1/include/stdbool.h \
+ /usr/include/newlib/stdio.h \
+ /usr/lib/gcc/arm-none-eabi/10.3.1/include/stdarg.h \
+ /usr/include/newlib/sys/types.h /usr/include/newlib/sys/_stdint.h \
+ /usr/include/newlib/machine/endian.h \
+ /usr/include/newlib/machine/_endian.h /usr/include/newlib/sys/select.h \
+ /usr/include/newlib/sys/_sigset.h /usr/include/newlib/sys/_timeval.h \
+ /usr/include/newlib/sys/timespec.h /usr/include/newlib/sys/_timespec.h \
+ /usr/include/newlib/sys/_pthreadtypes.h /usr/include/newlib/sys/sched.h \
+ /usr/include/newlib/machine/types.h /usr/include/newlib/sys/stdio.h \
+ /home/student/Desktop/final_files1/tmp/STM32CubeIDe/WIFI_OTA_2/Inc/es_wifi_conf.h \
+ /home/student/Desktop/final_files1/tmp/STM32CubeIDe/WIFI_OTA_2/Common/Inc/es_wifi_io.h \
+ /home/student/Desktop/final_files1/tmp/STM32CubeIDe/WIFI_OTA_2/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal.h \
+ /home/student/Desktop/final_files1/tmp/STM32CubeIDe/WIFI_OTA_2/Inc/stm32l4xx_hal_conf.h \
+ /home/student/Desktop/final_files1/tmp/STM32CubeIDe/WIFI_OTA_2/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_rcc.h \
+ /home/student/Desktop/final_files1/tmp/STM32CubeIDe/WIFI_OTA_2/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_def.h \
+ /home/student/Desktop/final_files1/tmp/STM32CubeIDe/WIFI_OTA_2/Drivers/CMSIS/Device/ST/STM32L4xx/Include/stm32l4xx.h \
+ /home/student/Desktop/final_files1/tmp/STM32CubeIDe/WIFI_OTA_2/Drivers/CMSIS/Device/ST/STM32L4xx/Include/stm32l475xx.h \
+ /home/student/Desktop/final_files1/tmp/STM32CubeIDe/WIFI_OTA_2/Drivers/CMSIS/Include/core_cm4.h \
+ /home/student/Desktop/final_files1/tmp/STM32CubeIDe/WIFI_OTA_2/Drivers/CMSIS/Include/cmsis_version.h \
+ /home/student/Desktop/final_files1/tmp/STM32CubeIDe/WIFI_OTA_2/Drivers/CMSIS/Include/cmsis_compiler.h \
+ /home/student/Desktop/final_files1/tmp/STM32CubeIDe/WIFI_OTA_2/Drivers/CMSIS/Include/cmsis_gcc.h \
+ /home/student/Desktop/final_files1/tmp/STM32CubeIDe/WIFI_OTA_2/Drivers/CMSIS/Include/mpu_armv7.h \
+ /home/student/Desktop/final_files1/tmp/STM32CubeIDe/WIFI_OTA_2/Drivers/CMSIS/Device/ST/STM32L4xx/Include/system_stm32l4xx.h \
+ /home/student/Desktop/final_files1/tmp/STM32CubeIDe/WIFI_OTA_2/Drivers/STM32L4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
+ /home/student/Desktop/final_files1/tmp/STM32CubeIDe/WIFI_OTA_2/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_rcc_ex.h \
+ /home/student/Desktop/final_files1/tmp/STM32CubeIDe/WIFI_OTA_2/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_gpio.h \
+ /home/student/Desktop/final_files1/tmp/STM32CubeIDe/WIFI_OTA_2/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_gpio_ex.h \
+ /home/student/Desktop/final_files1/tmp/STM32CubeIDe/WIFI_OTA_2/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_dma.h \
+ /home/student/Desktop/final_files1/tmp/STM32CubeIDe/WIFI_OTA_2/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_cortex.h \
+ /home/student/Desktop/final_files1/tmp/STM32CubeIDe/WIFI_OTA_2/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_flash.h \
+ /home/student/Desktop/final_files1/tmp/STM32CubeIDe/WIFI_OTA_2/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_flash_ex.h \
+ /home/student/Desktop/final_files1/tmp/STM32CubeIDe/WIFI_OTA_2/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_flash_ramfunc.h \
+ /home/student/Desktop/final_files1/tmp/STM32CubeIDe/WIFI_OTA_2/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_i2c.h \
+ /home/student/Desktop/final_files1/tmp/STM32CubeIDe/WIFI_OTA_2/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_i2c_ex.h \
+ /home/student/Desktop/final_files1/tmp/STM32CubeIDe/WIFI_OTA_2/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_pwr.h \
+ /home/student/Desktop/final_files1/tmp/STM32CubeIDe/WIFI_OTA_2/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_pwr_ex.h \
+ /home/student/Desktop/final_files1/tmp/STM32CubeIDe/WIFI_OTA_2/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_spi.h \
+ /home/student/Desktop/final_files1/tmp/STM32CubeIDe/WIFI_OTA_2/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_spi_ex.h \
+ /home/student/Desktop/final_files1/tmp/STM32CubeIDe/WIFI_OTA_2/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_uart.h \
+ /home/student/Desktop/final_files1/tmp/STM32CubeIDe/WIFI_OTA_2/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_uart_ex.h \
+ /home/student/Desktop/final_files1/tmp/STM32CubeIDe/WIFI_OTA_2/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_hcd.h \
+ /home/student/Desktop/final_files1/tmp/STM32CubeIDe/WIFI_OTA_2/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_ll_usb.h \
+ /home/student/Desktop/final_files1/tmp/STM32CubeIDe/WIFI_OTA_2/Common/Inc/es_wifi.h
---
+++ Application/NewFirmware/WIFI_OTA_2/Build_Wifi_Ota/CMakeFiles/WIFI_OTA_2.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for WIFI_OTA_2.
+# This may be replaced when dependencies are built.
