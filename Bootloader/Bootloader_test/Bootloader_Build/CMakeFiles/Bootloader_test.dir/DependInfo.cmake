991d23065b62bfeb7494e6093f7aade2d7dedd6f
NiklaMag <nm011905830@fer.hr>
2025-09-22 09:02:49 -0400

    Add Bootloader_test (STM32CubeIDE project) under Bootloader

---
+++ Bootloader/Bootloader_test/Bootloader_Build/CMakeFiles/Bootloader_test.dir/DependInfo.cmake
@@ -0,0 +1,68 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "/home/student/Desktop/final_files1/tmp/STM32CubeIDe/Bootloader_test/Core/Startup/startup_stm32l475vgtx.s" "/home/student/Desktop/final_files1/tmp/STM32CubeIDe/Bootloader_test/Bootloader_Build/CMakeFiles/Bootloader_test.dir/Core/Startup/startup_stm32l475vgtx.s.obj"
+  )
+set(CMAKE_ASM_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_ASM
+  "STM32L475xx"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  "../Core/Inc"
+  "../Drivers/CMSIS/Device/ST/STM32L4xx/Include"
+  "../Drivers/CMSIS/Include"
+  "../Drivers/STM32L4xx_HAL_Driver/Inc"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/student/Desktop/final_files1/tmp/STM32CubeIDe/Bootloader_test/Core/Src/main.c" "CMakeFiles/Bootloader_test.dir/Core/Src/main.c.obj" "gcc" "CMakeFiles/Bootloader_test.dir/Core/Src/main.c.obj.d"
+  "/home/student/Desktop/final_files1/tmp/STM32CubeIDe/Bootloader_test/Core/Src/stm32l4xx_hal_msp.c" "CMakeFiles/Bootloader_test.dir/Core/Src/stm32l4xx_hal_msp.c.obj" "gcc" "CMakeFiles/Bootloader_test.dir/Core/Src/stm32l4xx_hal_msp.c.obj.d"
+  "/home/student/Desktop/final_files1/tmp/STM32CubeIDe/Bootloader_test/Core/Src/stm32l4xx_it.c" "CMakeFiles/Bootloader_test.dir/Core/Src/stm32l4xx_it.c.obj" "gcc" "CMakeFiles/Bootloader_test.dir/Core/Src/stm32l4xx_it.c.obj.d"
+  "/home/student/Desktop/final_files1/tmp/STM32CubeIDe/Bootloader_test/Core/Src/syscalls.c" "CMakeFiles/Bootloader_test.dir/Core/Src/syscalls.c.obj" "gcc" "CMakeFiles/Bootloader_test.dir/Core/Src/syscalls.c.obj.d"
+  "/home/student/Desktop/final_files1/tmp/STM32CubeIDe/Bootloader_test/Core/Src/sysmem.c" "CMakeFiles/Bootloader_test.dir/Core/Src/sysmem.c.obj" "gcc" "CMakeFiles/Bootloader_test.dir/Core/Src/sysmem.c.obj.d"
+  "/home/student/Desktop/final_files1/tmp/STM32CubeIDe/Bootloader_test/Core/Src/system_stm32l4xx.c" "CMakeFiles/Bootloader_test.dir/Core/Src/system_stm32l4xx.c.obj" "gcc" "CMakeFiles/Bootloader_test.dir/Core/Src/system_stm32l4xx.c.obj.d"
+  "/home/student/Desktop/final_files1/tmp/STM32CubeIDe/Bootloader_test/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal.c" "CMakeFiles/Bootloader_test.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal.c.obj" "gcc" "CMakeFiles/Bootloader_test.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal.c.obj.d"
+  "/home/student/Desktop/final_files1/tmp/STM32CubeIDe/Bootloader_test/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_cortex.c" "CMakeFiles/Bootloader_test.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_cortex.c.obj" "gcc" "CMakeFiles/Bootloader_test.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_cortex.c.obj.d"
+  "/home/student/Desktop/final_files1/tmp/STM32CubeIDe/Bootloader_test/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_dfsdm.c" "CMakeFiles/Bootloader_test.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_dfsdm.c.obj" "gcc" "CMakeFiles/Bootloader_test.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_dfsdm.c.obj.d"
+  "/home/student/Desktop/final_files1/tmp/STM32CubeIDe/Bootloader_test/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_dma.c" "CMakeFiles/Bootloader_test.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_dma.c.obj" "gcc" "CMakeFiles/Bootloader_test.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_dma.c.obj.d"
+  "/home/student/Desktop/final_files1/tmp/STM32CubeIDe/Bootloader_test/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_dma_ex.c" "CMakeFiles/Bootloader_test.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_dma_ex.c.obj" "gcc" "CMakeFiles/Bootloader_test.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_dma_ex.c.obj.d"
+  "/home/student/Desktop/final_files1/tmp/STM32CubeIDe/Bootloader_test/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_exti.c" "CMakeFiles/Bootloader_test.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_exti.c.obj" "gcc" "CMakeFiles/Bootloader_test.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_exti.c.obj.d"
+  "/home/student/Desktop/final_files1/tmp/STM32CubeIDe/Bootloader_test/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_flash.c" "CMakeFiles/Bootloader_test.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_flash.c.obj" "gcc" "CMakeFiles/Bootloader_test.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_flash.c.obj.d"
+  "/home/student/Desktop/final_files1/tmp/STM32CubeIDe/Bootloader_test/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_flash_ex.c" "CMakeFiles/Bootloader_test.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_flash_ex.c.obj" "gcc" "CMakeFiles/Bootloader_test.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_flash_ex.c.obj.d"
+  "/home/student/Desktop/final_files1/tmp/STM32CubeIDe/Bootloader_test/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_flash_ramfunc.c" "CMakeFiles/Bootloader_test.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_flash_ramfunc.c.obj" "gcc" "CMakeFiles/Bootloader_test.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_flash_ramfunc.c.obj.d"
+  "/home/student/Desktop/final_files1/tmp/STM32CubeIDe/Bootloader_test/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_gpio.c" "CMakeFiles/Bootloader_test.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_gpio.c.obj" "gcc" "CMakeFiles/Bootloader_test.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_gpio.c.obj.d"
+  "/home/student/Desktop/final_files1/tmp/STM32CubeIDe/Bootloader_test/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_i2c.c" "CMakeFiles/Bootloader_test.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_i2c.c.obj" "gcc" "CMakeFiles/Bootloader_test.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_i2c.c.obj.d"
+  "/home/student/Desktop/final_files1/tmp/STM32CubeIDe/Bootloader_test/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_i2c_ex.c" "CMakeFiles/Bootloader_test.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_i2c_ex.c.obj" "gcc" "CMakeFiles/Bootloader_test.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_i2c_ex.c.obj.d"
+  "/home/student/Desktop/final_files1/tmp/STM32CubeIDe/Bootloader_test/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_pcd.c" "CMakeFiles/Bootloader_test.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_pcd.c.obj" "gcc" "CMakeFiles/Bootloader_test.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_pcd.c.obj.d"
+  "/home/student/Desktop/final_files1/tmp/STM32CubeIDe/Bootloader_test/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_pcd_ex.c" "CMakeFiles/Bootloader_test.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_pcd_ex.c.obj" "gcc" "CMakeFiles/Bootloader_test.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_pcd_ex.c.obj.d"
+  "/home/student/Desktop/final_files1/tmp/STM32CubeIDe/Bootloader_test/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_pwr.c" "CMakeFiles/Bootloader_test.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_pwr.c.obj" "gcc" "CMakeFiles/Bootloader_test.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_pwr.c.obj.d"
+  "/home/student/Desktop/final_files1/tmp/STM32CubeIDe/Bootloader_test/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_pwr_ex.c" "CMakeFiles/Bootloader_test.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_pwr_ex.c.obj" "gcc" "CMakeFiles/Bootloader_test.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_pwr_ex.c.obj.d"
+  "/home/student/Desktop/final_files1/tmp/STM32CubeIDe/Bootloader_test/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_qspi.c" "CMakeFiles/Bootloader_test.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_qspi.c.obj" "gcc" "CMakeFiles/Bootloader_test.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_qspi.c.obj.d"
+  "/home/student/Desktop/final_files1/tmp/STM32CubeIDe/Bootloader_test/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_rcc.c" "CMakeFiles/Bootloader_test.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_rcc.c.obj" "gcc" "CMakeFiles/Bootloader_test.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_rcc.c.obj.d"
+  "/home/student/Desktop/final_files1/tmp/STM32CubeIDe/Bootloader_test/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_rcc_ex.c" "CMakeFiles/Bootloader_test.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_rcc_ex.c.obj" "gcc" "CMakeFiles/Bootloader_test.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_rcc_ex.c.obj.d"
+  "/home/student/Desktop/final_files1/tmp/STM32CubeIDe/Bootloader_test/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_spi.c" "CMakeFiles/Bootloader_test.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_spi.c.obj" "gcc" "CMakeFiles/Bootloader_test.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_spi.c.obj.d"
+  "/home/student/Desktop/final_files1/tmp/STM32CubeIDe/Bootloader_test/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_spi_ex.c" "CMakeFiles/Bootloader_test.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_spi_ex.c.obj" "gcc" "CMakeFiles/Bootloader_test.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_spi_ex.c.obj.d"
+  "/home/student/Desktop/final_files1/tmp/STM32CubeIDe/Bootloader_test/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_uart.c" "CMakeFiles/Bootloader_test.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_uart.c.obj" "gcc" "CMakeFiles/Bootloader_test.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_uart.c.obj.d"
+  "/home/student/Desktop/final_files1/tmp/STM32CubeIDe/Bootloader_test/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_uart_ex.c" "CMakeFiles/Bootloader_test.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_uart_ex.c.obj" "gcc" "CMakeFiles/Bootloader_test.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_uart_ex.c.obj.d"
+  "/home/student/Desktop/final_files1/tmp/STM32CubeIDe/Bootloader_test/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_ll_usb.c" "CMakeFiles/Bootloader_test.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_ll_usb.c.obj" "gcc" "CMakeFiles/Bootloader_test.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_ll_usb.c.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
